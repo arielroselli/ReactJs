845435c926e0233c230b8981e86bc5c593248a36
Ariel Roselli <arieelroselli@gmail.com>
2022-04-04 20:14:04 -0300

    ItemCount sumar restar, DrowpDown, carrito navbar

---
+++ mesero-digital/src/components/ItemListContainer/index.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import ItemCount from "../ItemCount";
+
+
+const ItemListContainer = () => {
+    const [count, setCount] = useState(1);
+
+    const onAdd = (condition) => {
+        if(condition === '-'){
+            setCount(count - 1);
+        }
+        if(condition === '+'){
+            setCount(count + 1);
+        }
+
+    };
+
+const stock = 5;
+const initial = 1;
+
+  return (
+      <div>
+          <ItemCount onAdd={onAdd} stock={stock} initial={initial} count={count}/>
+      </div>
+  );
+};
+
+export default ItemListContainer    
